2f9e2e620ce1360dc8fd7819294ecfcf706b7136
Biruh Alemayehu <alemayehubiruh@gmail.com>
2025-02-09 23:31:05 +0300

    url
    change

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,6 @@ from core.license import request_license, validate_license
 
 urlpatterns = [
     path("verify-update-balance/", verify_update_balance, name="verify_update_balance"),
-    path("request/", request_license, name="request_license"),
+    path("license-request/", request_license, name="request_license"),
     path("validate/<str:license_key>/<str:machine_id>/", validate_license, name="validate_license"),
 ]
